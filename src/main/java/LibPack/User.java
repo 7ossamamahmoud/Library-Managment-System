e4a50ac88431a3883d17e5cd80e15fb0fe2b2105
Ossama Mahmoud <70413399+7ossamamahmoud@users.noreply.github.com>
2022-02-12 05:02:35 +0200

    Add files via upload

---
+++ src/main/java/LibPack/User.java
@@ -0,0 +1,16 @@
+package LibPack;
+/**
+ *
+ * @author Ossama Mahmoud
+ */
+public interface User {
+
+    public int searchBook(int book_id);
+
+    public String Search_For_Book(int bookId);
+
+    public int searchUser(int user_id);
+
+    public void rentBook();
+}
+
